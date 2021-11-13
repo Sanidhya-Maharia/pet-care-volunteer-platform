b09e930328a64dcbf9ecf304f449a8a8d6b178b7
Sanidhya Maharia <61234539+Sanidhya-Maharia@users.noreply.github.com>
2021-11-13 21:08:11 +0530

    Sign up page final test version
    i have depression

--- Backend/app.py
+++ Backend/app.py
@@ -5,24 +5,51 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('login.html')
+    return redirect('/login')
 
-@app.route("/loggedin",methods = ['POST', 'GET'])
-def display():
-    name = request.form['Name']
-    password = request.form['Password']
-    result = Login.check(name, password)
-    if result!= None:
-        return result
-    else:
-        return redirect('/')
+@app.route("/login",methods = ['POST', 'GET'])
+def loginpage():
+    if request.method == 'POST':
+        name = request.form['Name']
+        password = request.form['Password']
+        result = Login.check(name, password)
+        if result != None:
+            return result
+        else:
+            return redirect('/login')
+    
+    return render_template('login.html')
 
 @app.route("/signup",methods = ['POST', 'GET'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'POST':
+        name = request.form['signupname']
+        password = request.form['signuppass']
+        password2 = request.form['signuprepass']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        pincode = request.form['pincode']
+        city = request.form['city']
+        phno = request.form['phno']
+        email = request.form['email']
 
-@app.route("/signup/profile",methods = ['POST', 'GET'])
-def profile():
-    name = request.form['signupname']
-    result = SignUp.check(name)
-    return result
+        List = [name, password, password2, firstname, lastname, pincode, city, phno, email]
+
+        result1 = SignUp.check_if_unique(name)
+        if result1 != None:
+            return redirect('/signup')
+        
+        result2 = SignUp.check_password(password, password2)
+        if result2 == None:
+            return redirect('/signup')
+
+        for x in List:
+            result3 = SignUp.check_not_null(x)
+            if result3 == None:
+                return redirect('/signup')
+        
+        SignUp.updatelogindb(name, password)
+        SignUp.updateprofiledb(name, firstname, lastname, pincode, city, phno, email)
+        return "accepted"
+
+    return render_template('signup.html')
--- Database_Handler/handler.py
+++ Database_Handler/handler.py
@@ -3,6 +3,7 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://Sanidhya:user1@cluster0.9uhlx.mongodb.net/User_info?retryWrites=true&w=majority")
 db = client["User_info"]
 login = db["Login"]
+profile = db["Profile"]
 
 class Login:
     def check(name, password):
@@ -11,10 +12,29 @@ class Login:
         return x
 
 class SignUp:
-    def check(name):
+    def check_if_unique(name):
         q = {"name": name}
         x = login.find_one(q, {"_id": 0})
-        if x!=None:
-            return "username already exists"
+        return x
+
+    def check_password(password1, password2):
+        if len(password1)<5:
+            return None
+        elif password1!=password2:
+            return None
         else:
-            return "accepted"
+            return 1
+
+    def check_not_null(var):
+        if var==None:
+            return None
+        else:
+            return 1
+
+    def updatelogindb(name , password):
+        q = {"name": name, "password": password}
+        login.insert_one(q)
+
+    def updateprofiledb(name, firstname, lastname, pincode, city, phno, email):
+        q = {"_id": name, "firstname": firstname, "lastname": lastname, "pincode": pincode, "city": city, "phno": phno, "email": email}
+        profile.insert_one(q)
